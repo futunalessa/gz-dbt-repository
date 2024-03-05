a102cf12cf2b34a35ccd061918413c9acb778b3c
futunalessa@gmail.com <futunalessa@gmail.com>
2024-03-05 14:06:56 +0000

    cast

--- models/int_sales_margin.sql/intermediate.sql
+++ models/int_sales_margin.sql/intermediate.sql
@@ -5,8 +5,8 @@ orders_id,
 quantity, 
 date_date,
 purchase_price,
-round(quantity * purchase_price,2) as purchase_cost ,
-round(revenue-(quantity*purchase_cost),2) as margin
+round(quantity * purchase_price,2) as purchase_cost,
+round(revenue-(quantity*cast(purchase_costas float64)),2) as margin
 from {{ref("stg_gz_raw_data_sales")}}
 join {{ref("stg_gz_raw_data_product")}}
 using(product_id)
