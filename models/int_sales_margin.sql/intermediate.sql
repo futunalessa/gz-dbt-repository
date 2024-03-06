8b58025077d2f38a9b2561b872cb047d12612b69
futun alessa <futunalessa@gmail.com>
2024-03-06 08:14:55 +0000

    done

--- models/int_sales_margin.sql/intermediate.sql
+++ models/int_sales_margin.sql/intermediate.sql
@@ -4,9 +4,9 @@ SELECT
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchSE_PRICE AS FLOAT64), 
-    ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS margin
-FROM {{ref("stg_gz_raw_data__raw_gz_sales")}} s
-LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_product")}} p 
- on s.pdt_id=p.products_id
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+ on s.product_id=p.products_id
